977f63fee8617b4b2eebe4e100b4b2279bb89a4f
gitahievans <evanygk@gmail.com>
2022-06-24 14:31:37 +0300

    Added audio button and image

--- Development/Code/phase1/Phase-1-final-project/index.js
+++ Development/Code/phase1/Phase-1-final-project/index.js
@@ -3,7 +3,8 @@ const btn = document.querySelector("button")
 const wordSearched = document.querySelector(".word p")
 const meaning = document.querySelector(".meaning p")
 const example = document.querySelector(".example p")
-
+const mic = document.querySelector(".voice button")
+ 
 function searchWord(){
 document.addEventListener("DOMContentLoaded", () => { 
 
@@ -13,13 +14,42 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
         .then(response => response.json())
         .then(function(wordData){ 
+            console.log(wordData)
             wordSearched.textContent = wordData[0].word  
+            let def = wordData[0].meanings[0].definitions[0].definition
             
-            meaning.textContent = wordData[0].meanings[0].definitions[0].definition 
-           
-            example.textContent = wordData[0].meanings[0].definitions[0].example
+           meaning.textContent = `${def}`
+
+           example.textContent = wordData[0].meanings[0].definitions[0].example
+
+
+           })
         })
-})
+    
 })
 }
 searchWord()
+
+function playAudio(){
+    let audio = new Audio(`https:${wordData[0].phonetics[0].audio}`)
+    audio.play()
+}
+
+playAudio()
+
+
+
+
+
+// let def2 = wordData[0].meanings[0].definitions[1].definition
+// let def3 = wordData[0].meanings[0].definitions[2].definition
+// if (def3 === true){ 
+//     meaning.textContent = `1:${def1} 2:${def2} 3:${def3}`
+//    }
+//    else if(def2 === true){
+//     meaning.textContent = `1:${def1} 2:${def2}`
+//    }
+//    else
+//    {
+//     meaning.textContent = `${def1}`
+//    }
