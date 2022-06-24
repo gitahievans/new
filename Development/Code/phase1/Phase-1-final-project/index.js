afa96236f23b8aad8463f37774c7ed19f2618062
gitahievans <evanygk@gmail.com>
2022-06-24 15:51:16 +0300

    Made styling changes on header

--- Development/Code/phase1/Phase-1-final-project/index.js
+++ Development/Code/phase1/Phase-1-final-project/index.js
@@ -1,9 +1,9 @@
-const btn = document.querySelector("button")
+const btn = document.querySelector("button#search")
 
 const wordSearched = document.querySelector(".word p")
 const meaning = document.querySelector(".meaning p")
 const example = document.querySelector(".example p")
-const mic = document.querySelector(".voice button")
+const sound = document.querySelector(".voice button")
  
 function searchWord(){
 document.addEventListener("DOMContentLoaded", () => { 
@@ -14,28 +14,28 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
         .then(response => response.json())
         .then(function(wordData){ 
-            console.log(wordData)
+            
             wordSearched.textContent = wordData[0].word  
             let def = wordData[0].meanings[0].definitions[0].definition
             
-           meaning.textContent = `${def}`
+            meaning.textContent = `${def}`
 
-           example.textContent = wordData[0].meanings[0].definitions[0].example
+            example.textContent = wordData[0].meanings[0].definitions[0].example
 
-
-           })
+            sound.setAttribute("src", `https:${wordData[0].phonetics[0].audio}`)
         })
-    
+       
+       })
 })
 }
+
 searchWord()
 
+
 function playAudio(){
-    let audio = new Audio(`https:${wordData[0].phonetics[0].audio}`)
-    audio.play()
+    sound.play();
 }
 
-playAudio()
 
 
 
