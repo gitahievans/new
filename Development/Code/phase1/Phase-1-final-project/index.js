5eecfe23d41fa9eb353dafac2cf08becf3eac219
gitahievans <evanygk@gmail.com>
2022-06-30 19:55:40 +0300

    Added a wordDetails section to give type of word and synonyms of word.

--- Development/Code/phase1/Phase-1-final-project/index.js
+++ Development/Code/phase1/Phase-1-final-project/index.js
@@ -1,30 +1,12 @@
-const btn = document.querySelector("button#search")
-const wordSearched = document.querySelector(".word p")
-const meaning = document.querySelector(".meaning p")
-const example = document.querySelector(".example p")
-let voice = document.querySelector(".voice button")
-
-// async function fetchData(word){
-//     btn.addEventListener(("click"), (e) => {
-//         let word = document.querySelector("#to-search").value
-//         fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-//         .then(response => response.json())
-//         .then(wordData =>  
-//             addToNode(wordData))
-//             document.getElementById("to-search").value = ' '
-//             })
-//         }
-//   fetchData()
-  
-
-// function addToNode(wordData){
-//     wordSearched.textContent = wordData[0].word
-//     let def = wordData[0].meanings[0].definitions[0].definition
-//     meaning.textContent = `${def}`
-//     example.textContent = wordData[0].meanings[0].definitions[0].example
-// }
-
-let input = document.querySelector("#to-search")
+document.addEventListener("DOMContentLoaded", () => {
+   
+    const btn = document.querySelector("button#search")
+    const wordSearched = document.querySelector(".word p")
+    const meaning = document.querySelector(".meaning p")
+    const example = document.querySelector(".example p")
+    let voice = document.querySelector(".voice button")
+    let wordDetails = document.querySelector(".details p")
+    let input = document.querySelector("#to-search")
 
 fetchData("love")
 async function fetchData(word){
@@ -38,11 +20,11 @@ btn.addEventListener("click", addDictionaryInfo)
 
 async function addDictionaryInfo(){
     const data = await fetchData(input.value)
-    console.log(data);
-
     wordSearched.textContent = data.word
     meaning.textContent = data.meanings[0].definitions[0].definition
     example.textContent = data.meanings[0].definitions[0].example
+    
+    wordDetails.textContent = `WORD TYPE: ${data.meanings[0].partOfSpeech} SYNOYMS: ${data.meanings[0].synonyms}`
 }
 
 voice.addEventListener("click", pronounce)
@@ -54,45 +36,4 @@ async function pronounce(){
     voice.play()
 }
 
-
-
-
-
-
-// function addToNode(wordData){
-//     wordSearched.textContent = wordData[0].word
-//     let def = wordData[0].meanings[0].definitions[0].definition
-//     meaning.textContent = `${def}`
-//     example.textContent = wordData[0].meanings[0].definitions[0].example
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// wordProperties.addEventListener("mouseover", e => {
-//                 alert("you're moving")
-//                 wordProperties.style.backgroundColor = "grey";
-//             })
-
-
-
-    // wordSearched.addEventListener("click", e => {
-    //     console.log("the mouse is over me")
-    // })
-    // wordSearched.addEventListener("mouseover", e => {
-    //     console.log("the mouse is not over me")
-    // })
+})
